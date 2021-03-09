f8e1621c660c9819b8dfbdd23e9a08462ee832de
yyxiaoqing <yyxiaoqing@gmail.com>
2021-03-09 15:45:58 +0800

    Update youth.js

--- youth.js
+++ youth.js
@@ -8,10 +8,10 @@
 
 
 const $ = new Env("中青看点")
-let notifyInterval = $.getdata("notifytimes")||50 //通知间隔，默认抽奖每50次通知一次，如需关闭全部通知请设为0
+let notifyInterval = $.getdata("notifytimes")||30 //通知间隔，默认抽奖每50次通知一次，如需关闭全部通知请设为0
 let s = $.getdata('delay_rotary_zq')||"10" //转盘延迟时间
 const notify = $.isNode() ? require('./sendNotify') : '';
-const ONCard = $.getdata('zqcard')||"false" //早起打卡开关
+const ONCard = $.getdata('zqcard')||"true" //早起打卡开关
 const withdrawcash = $.getdata('zqcash')||30 //提现金额
 let withdrawUrl = $.getdata('cashurl_zq');
 let withdrawBody = $.getdata('cashbody_zq');
